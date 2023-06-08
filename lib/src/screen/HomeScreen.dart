a21507f54bf3c20f279a17e52cd4dfc54d62a8b9
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-06-08 11:47:51 +0530

    layout

--- lib/src/screen/HomeScreen.dart
+++ lib/src/screen/HomeScreen.dart
@@ -5,6 +5,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Project"),
@@ -16,6 +18,62 @@ class HomeScreen extends StatelessWidget {
         actions: const [Icon(Icons.card_travel), Icon(Icons.notifications)],
       ),
       backgroundColor: const Color.fromARGB(255, 244, 224, 3),
+      body: Column(
+        children: [
+          Container(
+            height: height * .88,
+            width: width,
+            color: Color.fromARGB(255, 250, 249, 249),
+            child: Column(children: [
+              Container(
+                height: 300,
+                width: width,
+                color: Color.fromARGB(255, 7, 255, 110),
+                child: Row(children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Color.fromARGB(255, 197, 255, 7),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Color.fromARGB(255, 90, 7, 255),
+                      )
+                    ],
+                  )
+                ]),
+              ),
+              /*   Container(
+                height: 200,
+                width: width,
+                margin: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 11, 7, 255),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(100),
+                      bottomRight: Radius.circular(100)),
+                ),
+              ), */
+              /*  Container(
+                height: 200,
+                width: width,
+                margin: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 11, 7, 255),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ), */
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
